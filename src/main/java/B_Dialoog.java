63624736deffc863f0f498fa2a1f60784cb7ec81
wesleydewinne <wesleydewinne@gmail.com>
2025-03-12 12:03:29 +0100

    voorbereiding les Methods

--- src/main/java/B_Dialoog.java
+++ src/main/java/B_Dialoog.java
@@ -7,22 +7,37 @@ public class B_Dialoog {
         // De volgende zin wordt steeds door de ander gesproken.
         // Vervang System.out.println() steeds door je eigen method.
 
-        String name1 = "Bert";
-        String name2 = "Ernie";
+        String hey = ("Hey"); //Bert //Ernie
+        // String hey = ("Hey");
+        String welkom = ("Welcome to this wonderful conversation"); //Bert
+        String welkom1 = ("Thnx, I'm looking forward to it"); //Ernie
+        String weather = ("How about this weather?"); //Bert
+        String cold = ("I really don't mind the cold...."); //Ernie
+        String program =("And did you see that random program on that random channel?"); //Bert
+        String channel = ("Well... I did watch a random program on a random channel..."); //Ernie
+        String bye = ("Thank you for a wonderful conversation!"); //Bert
+        String bye1 = ("Cya"); //Ernie
 
-        System.out.println("Hey"); //Bert
-        System.out.println("Hey"); //Ernie
-        System.out.println("Welcome to this wonderful conversation");
-        System.out.println("Thnx, I'm looking forward to it");
-        System.out.println("How about this weather?");
-        System.out.println("I really don't mind the cold....");
-        System.out.println("And did you see that random program on that random channel?");
-        System.out.println("Well... I did watch a random program on a random channel...");
-        System.out.println("Thank you for a wonderful conversation!");
-        System.out.println("Cya");
+        bertSays(hey);
+        ernieSays(hey);
+        bertSays(welkom);
+        ernieSays(welkom1);
+        bertSays(weather);
+        ernieSays(cold);
+        bertSays(program);
+        ernieSays(channel);
+        bertSays(bye);
+        ernieSays(bye1);
     }
 
     // nieuwe methods komen hier
 
+    public static void bertSays(String text) {
+        System.out.println("Bert says: " + text );
+    }
+
+    public static void ernieSays(String text) {
+        System.out.println(text + " says Ernie");
+    }
 
 }
